fc50453a279674eda3da4f81c377059fccc91dbc
Matthew Leon <TreeSystemCaller@users.noreply.github.com>
2015-07-14 02:48:22 -0400

    Update helloWorld.asm

--- helloWorld.asm
+++ helloWorld.asm
@@ -1,18 +1,13 @@
 section     .text
-global      _start                              ;main declaration
-
+global      _start                              ;main routine init
 _start:                                         ;main routine
-
-    mov     edx,len                             ;message length
-    mov     ecx,msg                             ;message to write
-    mov     ebx,1                               ;pipe to standard out
-    mov     eax,4                               ;system call to write to eax
+    mov     edx,len                             ;move length of message to edx register
+    mov     ecx,msg                             ;move message to ecx register
+    mov     ebx,1                               ;move standard output to ebx register
+    mov     eax,4                               ;move message to ebx register
     int     0x80                                ;run system call to write messasge
-
     mov     eax,1                               ;system call to exit
     int     0x80                                ;run system call to exit
-
 section     .data
-
 msg     db  'Hello, world!',0xa           
 len     equ $ - msg                         
